7fb509ac6c5372a66102d338bf47b1f9a2fc68c4
Frankie-lii <oyiengofrankline49@gmail.com>
2025-11-06 12:32:23 -0800

    task 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+
+
 
 # 🟢 User Registration View
 def register_view(request):
