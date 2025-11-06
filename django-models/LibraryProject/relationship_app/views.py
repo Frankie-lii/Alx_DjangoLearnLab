e3acf1f2a697d9bce395c636b904bb533d0d7306
Frankie-lii <oyiengofrankline49@gmail.com>
2025-11-06 11:34:00 -0800

    task 1

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
+
+# Function-based view: list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+# Class-based view: display details of a specific library
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
