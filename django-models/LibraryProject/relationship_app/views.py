37b620395da53283448b36029322632c35683240
Frankie-lii <oyiengofrankline49@gmail.com>
2025-11-06 12:15:18 -0800

    task 1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from django.shortcuts import render
 from django.views.generic import DetailView   # ✅ For class-based view
-from .models import Book, Library              # ✅ Must include Library model
+from .models import Book, Library
+from .models import Library
 
 # ✅ Function-based view — list all books
 def list_books(request):
