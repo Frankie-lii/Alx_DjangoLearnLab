e2fe12ba61a54bad9b98d6653d22b25981746bb2
Frankie-lii <oyiengofrankline49@gmail.com>
2025-11-08 11:16:34 -0800

    task 3

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-class UserProfile(models.Model):", "Admin", "Members"
+
+# ✅ Create a UserProfile model that includes a role field
+class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
         ('Librarian', 'Librarian'),
@@ -15,9 +17,10 @@ class UserProfile(models.Model):", "Admin", "Members"
     def __str__(self):
         return f"{self.user.username} - {self.role}"
 
-# ✅ Automatically create a UserProfile when a User is created
+# ✅ Automatically create profile when a user is created
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
 
