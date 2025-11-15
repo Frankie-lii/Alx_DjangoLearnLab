fb6c5cd693620bead270cc74862919d5e33ec003
Frankie-lii <oyiengofrankline49@gmail.com>
2025-11-15 10:44:25 -0800

    task 3

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,36 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Book
-from .forms import SearchForm, BookForm
-from django.views.decorators.csrf import csrf_protect
+# LibraryProject/bookshelf/views.py
 
-@csrf_protect
-def book_list(request):
-    form = SearchForm(request.GET or None)
+from django.shortcuts import render
+from .forms import ExampleForm  # <-- Make sure this line exists
 
-    books = Book.objects.all()
-
-    # Secure search (no SQL injection)
-    if form.is_valid():
-        query = form.cleaned_data.get("query")
-        if query:
-            books = books.filter(title__icontains=query)
-
-    return render(request, "bookshelf/book_list.html", {"books": books, "form": form})
-
-
-@csrf_protect
-def add_book(request):
-    if request.method == "POST":
-        form = BookForm(request.POST)
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
         if form.is_valid():
-            form.save()
+            # process data here
+            pass
     else:
-        form = BookForm()
-
-    return render(request, "bookshelf/form_example.html", {"form": form})
-
-# Documentation:
-# - Django ORM prevents SQL injection
-# - CSRF protection applied using @csrf_protect
-# - User input validated using Django Forms
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
