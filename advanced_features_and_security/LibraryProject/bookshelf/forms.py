dd57219ee42bcbefd445b1407f9b96432dee9b57
Frankie-lii <oyiengofrankline49@gmail.com>
2025-11-15 10:40:34 -0800

    task 3

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,18 +1,8 @@
+# LibraryProject/bookshelf/forms.py
+
 from django import forms
-from .models import Book
 
-class SearchForm(forms.Form):
-    query = forms.CharField(
-        max_length=100,
-        required=False,
-        strip=True,
-        widget=forms.TextInput(attrs={"placeholder": "Search books..."})
-    )
-
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ["title", "author", "description"]
-
-# Forms automatically prevent SQL injection through Django ORM.
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
 
