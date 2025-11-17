722fbe424bb9f981a36a20e6ef7e7c22da29b04b
Frankie-lii <oyiengofrankline49@gmail.com>
2025-11-17 07:45:28 -0800

    task 0

---
+++ api_project/settings.py
@@ -0,0 +1,12 @@
+# api_project/settings.py
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',  # Add Django REST Framework
+    'api',  # We'll create this app in the next step
+]
