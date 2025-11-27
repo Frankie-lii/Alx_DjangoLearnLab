57cba8d2250a7404b723d3a54bec31f0399f9e1e
Frankie-lii <oyiengofrankline49@gmail.com>
2025-11-27 11:34:02 -0800

    task 0

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -31,4 +31,13 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.publication_year})"
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    publish_date = models.DateField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
