88283d147ac6c0deb35a8ce018b7f4299b7d40ee
Frankie-lii <oyiengofrankline49@gmail.com>
2025-12-08 07:48:54 -0800

    task 1

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -29,9 +29,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+    'django_filters',
     
-    # Local apps - ADDED 'accounts' HERE
+    # Local apps
     'accounts',
+    'posts',  # Make sure 'posts' is added here
 ]
 
 MIDDLEWARE = [
@@ -99,6 +101,13 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
     ],
 }
 
