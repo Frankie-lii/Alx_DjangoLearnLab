f88f2123401ed289d157938beda82af402d441e6
Frankie-lii <oyiengofrankline49@gmail.com>
2025-12-11 10:18:41 -0800

    task 2

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -4,13 +4,27 @@ from .views import (
     UserLoginView,
     UserLogoutView,
     UserProfileView,
-    UserDetailView
+    UserDetailView,
+    UserFollowView,
+    UserFollowingListView,
+    UserFollowersListView,
+    UserListView
 )
 
 urlpatterns = [
+    # Authentication endpoints
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
+    
+    # Profile endpoints
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('users/<str:username>/', UserDetailView.as_view(), name='user-detail'),
+    
+    # Follow management endpoints
+    path('follow/<int:user_id>/', UserFollowView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UserFollowView.as_view(), name='unfollow-user'),
+    path('following/', UserFollowingListView.as_view(), name='following-list'),
+    path('followers/', UserFollowersListView.as_view(), name='followers-list'),
+    path('users/', UserListView.as_view(), name='user-list'),
 ]
